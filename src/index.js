4d6dff743c33930ad57c1bda16a8efbd89ee8bf8
Sean McIntyre <smcintyre@uncharted.software>
2017-11-07 16:25:11 -0500

    Very basic scaffolding for initial development spike

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import './style/index.scss';
+import { Component } from 'preact';
+
+export default class App extends Component {
+  render () {
+    return (
+      <div>
+        <h1>Hello, World!</h1>
+      </div>
+    );
+  }
+}
