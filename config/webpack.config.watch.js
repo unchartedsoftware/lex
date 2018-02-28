144636c77326ed120aa649aec1b046579d673b9a
Christian Natis <cnatis@uncharted.software>
2018-02-28 11:02:45 -0500

    #85 - remove unused html-webpack-plugin

--- config/webpack.config.watch.js
+++ config/webpack.config.watch.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const fs = require('fs');
 
