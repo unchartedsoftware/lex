cf3ae94855ccfba61d15e6ba9bd858bb969aa1b9
Sean McIntyre <s.mcintyre@xverba.ca>
2018-04-09 14:39:03 -0400

    Removing unarchive functionality from rewind. Rewind no longer erases state data

--- src/lib/token-state-machine.js
+++ src/lib/token-state-machine.js
@@ -197,30 +197,24 @@ export class TokenStateMachine extends EventEmitter {
   }
 
   /**
-   * Pops a value from the current `State`'s archive, if possible, overwriting the current value. If the
-   * archive is empty, transitions to the parent state from the current state, regardless of whether or
-   * not the current state is valid, or has a parent. Resets the value of the current state before rewinding.
-   * If the current state has no parent, then this will only reset the value of the current state.
+   * Transitions to the parent state from the current state, regardless of whether or
+   * not the current state is valid, or has a parent. Leaves the value of the current state as-is,
+   * permitting a potential transition back to this state after editing a previous one.
    *
    * @returns {State} The new current state.
    */
   rewind () {
-    if (this.state.archive.length > 0) {
-      this.state.unarchiveValue();
-      this.emit('state changed', this.state, this.state);
+    if (this.state.parent) {
+      const oldState = this.state;
+      // oldState.reset();
+      _currentState.set(this, this.state.parent);
+      this.emit('state changed', this.state, oldState);
+    }
+    // If the new state is read-only, rewind past it automatically.
+    if (this.state.isReadOnly) {
+      return this.rewind();
     } else {
-      if (this.state.parent) {
-        const oldState = this.state;
-        oldState.reset();
-        _currentState.set(this, this.state.parent);
-        this.emit('state changed', this.state, oldState);
-      }
-      // If the new state is read-only, rewind past it automatically.
-      if (this.state.isReadOnly) {
-        return this.rewind();
-      } else {
-        return this.state;
-      }
+      return this.state;
     }
   }
 
