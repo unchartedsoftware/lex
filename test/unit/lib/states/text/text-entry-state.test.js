ffb4d69109157c8722908cdda61f00ae9eac6963
Daniela Baron <baron.daniela@gmail.com>
2018-05-03 14:01:18 -0400

    add text entry state validation tests

---
+++ test/unit/lib/states/text/text-entry-state.test.js
@@ -0,0 +1,58 @@
+// test dependencies
+import { expect } from 'chai';
+// system under test
+import { TextEntryState } from '../../../../../src/lib/states/text/text-entry-state';
+
+describe('TextEntryState', () => {
+  describe('isValid', () => {
+    it('Returns true if value contains a `key` property with greater than zero length', () => {
+      // Given
+      const config = {};
+      const textEntryState = new TextEntryState(config);
+      // When
+      textEntryState.value = {key: 'foo'};
+      // Then
+      expect(textEntryState.isValid).to.be.true;
+    });
+
+    it('Returns false if value does not contain a `key` property', () => {
+      // Given
+      const config = {};
+      const textEntryState = new TextEntryState(config);
+      // When
+      textEntryState.value = {something: 'foo'};
+      // Then
+      expect(textEntryState.isValid).to.be.false;
+    });
+
+    it('Returns false if value `key` property is not a string', () => {
+      // Given
+      const config = {};
+      const textEntryState = new TextEntryState(config);
+      // When
+      textEntryState.value = {key: 42};
+      // Then
+      expect(textEntryState.isValid).to.be.false;
+    });
+
+    it('Returns false if value `key` property is an empty string', () => {
+      // Given
+      const config = {};
+      const textEntryState = new TextEntryState(config);
+      // When
+      textEntryState.value = {key: ''};
+      // Then
+      expect(textEntryState.isValid).to.be.false;
+    });
+
+    it('Returns false if value `key` property is null', () => {
+      // Given
+      const config = {};
+      const textEntryState = new TextEntryState(config);
+      // When
+      textEntryState.value = {key: null};
+      // Then
+      expect(textEntryState.isValid).to.be.false;
+    });
+  });
+});
