beca5be482dad2e0b96a950b91fa961ba73b29f7
Daniela Baron <baron.daniela@gmail.com>
2018-05-03 13:17:28 -0400

    datetime test box unbox interactions

--- test/unit/lib/states/temporal/datetime-entry-state.test.js
+++ test/unit/lib/states/temporal/datetime-entry-state.test.js
@@ -4,7 +4,7 @@ import { expect } from 'chai';
 import { DateTimeEntryState } from '../../../../../src/lib/states/temporal/datetime-entry-state';
 
 describe('DateTimeEntryState', () => {
-  describe('unbox', () => {
+  describe('unboxValue', () => {
     it('Returns null when given a null date object', () => {
       // Given
       const config = {};
@@ -50,6 +50,7 @@ describe('DateTimeEntryState', () => {
       expect(result).to.equal('2018-Dec-17');
     });
 
+    // Should we expect a different result than default timezone?
     it('Returns a custom formatted date for a custom timezone', () => {
       // Given
       const config = {
@@ -62,6 +63,61 @@ describe('DateTimeEntryState', () => {
       const result = dateTimeEntryState.unboxValue(dateobj);
       // Then
       expect(result).to.equal('2018-Dec-17');
+    });
+  });
+
+  describe('boxValue', () => {
+    it('Returns a date object given a date string matching the default format, in the default timezone', () => {
+      // Given
+      const config = {};
+      const dateTimeEntryState = new DateTimeEntryState(config);
+      const datestring = '2018/12/17';
+      // When
+      const result = dateTimeEntryState.boxValue(datestring);
+      // Then
+      expect(typeof result).to.equal('object');
+      expect(result.getUTCFullYear()).to.equal(2018);
+      expect(result.getUTCMonth()).to.equal(11); // js months Jan to Dec -> 0 to 11
+      expect(result.getUTCDate()).to.equal(17);
+    });
+
+    it('Returns null when date string does not match default format', () => {
+      // Given
+      const config = {};
+      const dateTimeEntryState = new DateTimeEntryState(config);
+      const datestring = '2018-12-17';
+      // When
+      const result = dateTimeEntryState.boxValue(datestring);
+      // Then
+      expect(result).to.be.null;
+    });
+  });
+
+  describe('box unbox interactions', () => {
+    it('box -> unbox -> box returns first boxed result', () => {
+      // Given
+      const config = {};
+      const dateTimeEntryState = new DateTimeEntryState(config);
+      const datestring = '2018/12/17';
+      // When
+      const boxed = dateTimeEntryState.boxValue(datestring);
+      const unboxed = dateTimeEntryState.unboxValue(boxed);
+      const result = dateTimeEntryState.boxValue(unboxed);
+      // Then
+      expect(result).to.deep.equal(boxed);
+    });
+
+    it('unbox -> box -> unbox returns first unboxed result', () => {
+      // Given
+      const config = {};
+      const dateTimeEntryState = new DateTimeEntryState(config);
+      const dateobj = new Date('2018-12-17');
+      // When
+      const unboxed = dateTimeEntryState.unboxValue(dateobj);
+      const boxed = dateTimeEntryState.boxValue(unboxed);
+      const result = dateTimeEntryState.unboxValue(boxed);
+      // Then
+      expect(result).to.equal(unboxed);
     });
   });
 });
