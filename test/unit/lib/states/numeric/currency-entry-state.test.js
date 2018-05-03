b349a372fc25ab95a889b7c0a63870b5d57ace7b
Daniela Baron <baron.daniela@gmail.com>
2018-05-03 15:50:17 -0400

    currency entry state format tests

---
+++ test/unit/lib/states/numeric/currency-entry-state.test.js
@@ -0,0 +1,60 @@
+// test dependencies
+import { expect } from 'chai';
+// system under test
+import { CurrencyEntryState } from '../../../../../src/lib/states/numeric/currency-entry-state';
+
+describe('CurrencyEntryState', () => {
+  describe('unformatUnboxedValue', () => {
+    it('Removes `$` characters', () => {
+      // Given
+      const currencyEntryState = new CurrencyEntryState({});
+      const displayKey = '$123.45';
+      // When
+      const result = currencyEntryState.unformatUnboxedValue(displayKey);
+      // Then
+      expect(result).to.equal('123.45');
+    });
+
+    it('Removes `,` characters', () => {
+      // Given
+      const currencyEntryState = new CurrencyEntryState({});
+      const displayKey = '123,456';
+      // When
+      const result = currencyEntryState.unformatUnboxedValue(displayKey);
+      // Then
+      expect(result).to.equal('123456');
+    });
+  });
+
+  describe('formatUnboxedValue', () => {
+    it('Formats decimal numerical as currency', () => {
+      // Given
+      const currencyEntryState = new CurrencyEntryState({});
+      const key = '123.45';
+      // When
+      const result = currencyEntryState.formatUnboxedValue(key);
+      // Then
+      expect(result).to.equal('$123.45');
+    });
+
+    it('Formats number with thousands separator', () => {
+      // Given
+      const currencyEntryState = new CurrencyEntryState({});
+      const key = '123456';
+      // When
+      const result = currencyEntryState.formatUnboxedValue(key);
+      // Then
+      expect(result).to.equal('$123,456');
+    });
+
+    it('Returns original key when not a number', () => {
+      // Given
+      const currencyEntryState = new CurrencyEntryState({});
+      const key = 'abc';
+      // When
+      const result = currencyEntryState.formatUnboxedValue(key);
+      // Then
+      expect(result).to.equal('abc');
+    });
+  });
+});
