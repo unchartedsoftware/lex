1b04ce24fc5c2cb9bcc946799d7fdb2c2c5a186c
Daniela Baron <dbaron@uncharted.software>
2018-05-09 11:10:49 -0400

    test walking a simple tree from initial to terminal state

--- test/unit/lib/token-state-machine.test.js
+++ test/unit/lib/token-state-machine.test.js
@@ -9,21 +9,43 @@ import { TextEntryState } from '../../../src/lib/states/text/text-entry-state';
 describe('TokenStateMachine', () => {
   describe('transition', () => {
     it('Walks a simple tree', () => {
-      // Given
+      // Given a simple language
+      const opt1 = new OptionStateOption('First Name');
+      const opt2 = new OptionStateOption('Last Name');
       const stateTemplate = new StateTemplate(OptionState, {
         name: 'Choose a field to search',
-        options: [
-          new OptionStateOption('First Name'),
-          new OptionStateOption('Last Name') ],
+        options: [opt1, opt2],
         vkey: 'field'
       });
-      const language = stateTemplate.to('value', TextEntryState);
-      // When
+      const language = stateTemplate.to('value', TextEntryState, {name: 'Enter a string'});
+
+      // When machine is initialized with language root
       const tokenStateMachine = new TokenStateMachine(language.root);
-      // Then
-      expect(tokenStateMachine).to.not.be.undefined;
+
+      // Then verify expected root state
       expect(tokenStateMachine.id).to.be.finite;
       expect(tokenStateMachine.rootState.name).to.equal('Choose a field to search');
+      expect(tokenStateMachine.rootState.vkey).to.equal('field');
+
+      // Given a valid initial state
+      tokenStateMachine.rootState.options = [opt1, opt2];
+      tokenStateMachine.rootState.value = opt1;
+
+      // When transition is invoked
+      tokenStateMachine.transition();
+
+      // Then verify current state
+      expect(tokenStateMachine.state.name).to.equal('Enter a string');
+      expect(tokenStateMachine.state.vkey).to.equal('value');
+
+      // Ensure nextState is valid
+      tokenStateMachine.state.value = {key: 'Joe'};
+
+      // When another transition is invoked
+      tokenStateMachine.transition();
+
+      // Expect to be in terminal state
+      expect(tokenStateMachine.state.isTerminal).to.be.true;
     });
   });
 });
