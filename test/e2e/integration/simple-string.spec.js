62ce4a1597b016398de79d9b3592886fc7aa8f38
Daniela Baron <dbaron@uncharted.software>
2018-06-04 11:26:38 -0400

    introduce simple e2e test for simple string demo

---
+++ test/e2e/integration/simple-string.spec.js
@@ -0,0 +1,12 @@
+describe('Simple String', () => {
+  it('Select from list of predefined tokens and provide string values', () => {
+    cy.visit('http://localhost:8080/simple-string.html');
+
+    // Verify dropdown options
+    cy.get('[data-test=assistant-suggestion]').should('contain', 'First Name');
+    cy.get('[data-test=assistant-suggestion]').should('contain', 'Last Name');
+
+    // Typing a matching value should filter the dropdown list
+    cy.get('[data-test=lex-container] input').type('F');
+  });
+});
