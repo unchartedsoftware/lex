8bd8020c11127716b9f6464fd6c2c696a6107a14
Christian Natis <cnatis@uncharted.software>
2019-06-15 12:24:17 -0400

    #487 - Token should only request focus on state change when the state is dirty (not a state that has been reset)

--- src/components/token.jsx
+++ src/components/token.jsx
@@ -106,7 +106,12 @@ export class Token extends Component {
       stateArray: result
     });
     // this.setState({focused: true});
-    this.state.requestFocus();
+    if (result[0] && result[0].isDirty) {
+      // Only request focus when our first state is dirty
+      // This way we don't request focus when the state is reset
+      // We only want to request focus when the state change was a result of a transition
+      this.state.requestFocus();
+    }
   }
 
   get isBlank () {
