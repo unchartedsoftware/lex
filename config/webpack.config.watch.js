9eb681beefefdd144e7e2209aafa0f02a990abbc
Sean McIntyre <s.mcintyre@xverba.ca>
2019-11-07 13:58:59 -0500

    Updating all devDependencies

--- config/webpack.config.watch.js
+++ config/webpack.config.watch.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const webpack = require('webpack');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const fs = require('fs');
 
 const testFolder = path.resolve(__dirname, '../demo');
@@ -72,7 +72,7 @@ module.exports = {
     ]
   },
   plugins: [
-    new CleanWebpackPlugin(['dist']),
+    new CleanWebpackPlugin({cleanOnceBeforeBuildPatterns: path.join(process.cwd(), 'dist/**')}),
     new webpack.NamedModulesPlugin(),
     new webpack.HotModuleReplacementPlugin()
   ],
