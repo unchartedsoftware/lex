098e1b3a58dad158a5d4ef9ea4356ece2b4e29e0
Martin Matusiak <mmatusiak@uncharted.software>
2022-07-25 16:34:09 -0400

    chore: upgrade playwright to latest and fix configuration

--- playwright.config.js
+++ playwright.config.js
@@ -35,7 +35,7 @@ const config = {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://localhost:8081',
+    baseURL: 'http://localhost:8080',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'retain-on-failure',
@@ -96,14 +96,15 @@ const config = {
     // },
   ],
 
-  /* Folder for test artifacts such as screenshots, videos, traces, etc. */
-  outputDir: './tests/e2e/test-results/'
-
   /* Run your local dev server before starting the tests */
   // webServer: {
   //   command: 'npm run demo',
-  //   port: 8081
-  // }
+  //   port: 8080
+  // },
+
+  /* Folder for test artifacts such as screenshots, videos, traces, etc. */
+  outputDir: './tests/e2e/test-results/'
+
 };
 
 module.exports = config;
